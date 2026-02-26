60faecd9205a05a790a6050596d507af21109435
Sara0921 <salahsara360@gmail.com>
2026-02-26 21:10:24 +0200

    Question02

--- Assignment 1/Program.cs
+++ Assignment 1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Intrinsics.X86;
 using System.Security.Claims;
 using System.Xml.Linq;
 using static System.Runtime.InteropServices.JavaScript.JSType;
@@ -31,6 +32,26 @@ namespace Assignment_1
     //    }
     //}
     #endregion
+    #region Question02
+    //class Person
+    //{
+    //    public string Name;      // Anyone can access
+    //    private int age;         // Only this class can access
+
+    //    public void SetAge(int a)
+    //    {
+    //        if (a > 0)
+    //            age = a;
+    //    }
+
+    //    public int GetAge()
+    //    {
+    //        return age;
+    //    }
+    //}
+
+    #endregion
+
     #endregion
 
     internal class Program
@@ -38,7 +59,7 @@ namespace Assignment_1
         static void Main(string[] args)
         {
             #region Part 01
-            //#region Question01
+            #region Question01
             //// CLASS: Copy shares the same reference
             //PersonClass P01 = new PersonClass("Ahmed",20);
             //PersonClass P02 = P01;
@@ -68,8 +89,15 @@ namespace Assignment_1
 
 
             #endregion
+            #region Question02
+           // Person p = new Person();
 
+           // p.Name = "Ali";        //  Works — public
+           // p.SetAge(25);            //  Works — public method
+           // Console.WriteLine($"{p.Name} is {p.GetAge()} years old."); // p.age = 25;          // ❌ Error — private
+           //// public => anyone can use it. private => only the class itself can use it.
             #endregion
+        #endregion
         }
     }
 }
